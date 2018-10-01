3416ca520d6029876ca048ae3e65a2ca35544afa
Patrick Detlefsen <patrickdet@users.noreply.github.com>
2018-10-01 15:41:45 +0100

    1.0

--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule FuzzyCompare.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/patrickdet/fuzzy_compare"
+
   def project do
     [
       app: :fuzzy_compare,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A fuzzy string comparison library for Elixir",
+      source_url: @github_url,
+      homepage_url: @github_url,
+      package: [
+        maintainers: ["Patrick Detlefsen"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => @github_url
+        }
+      ],
+      docs: [
+        main: "FuzzyCompare"
+      ]
     ]
   end
 
